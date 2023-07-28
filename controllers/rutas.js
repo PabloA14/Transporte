9b78d8a2433c6199a542e79b280f929f010202c0
PabloA14 <138160222+PabloA14@users.noreply.github.com>
2023-07-28 14:21:14 -0500

    impementacion da la peeticion buscar rutas

--- controllers/rutas.js
+++ controllers/rutas.js
@@ -5,11 +5,27 @@ const httpRutas = {
         const ruta = await Ruta.find()
         res.json({ ruta })
     },
+
+
+    getRuta: async (req, res) => {
+        try {
+            const rutaBuscada = req.params.ruta;
+            const rutaEncontrado = await Ruta.findOne({ nombre: rutaBuscada });
+            if (!rutaEncontrado) {
+                return res.status(404).json({ mensaje: 'No se encontró el ruta con la cédula proporcionada.' });
+            }
+            res.json(rutaEncontrado);
+        } catch (error) {
+            console.error('Error al buscar el ruta:', error);
+            res.status(500).json({ mensaje: 'Hubo un error al buscar el ruta.' });
+        }
+    },
+
     postRuta: async (req, res) => {
-        const { origen, destino, valor_pasage } = req.body
-        const ruta = await Ruta({ origen, destino, valor_pasage })
+        const { nombre, origen, destino, valor_pasage } = req.body
+        const ruta = await Ruta({ nombre, origen, destino, valor_pasage })
         await ruta.save()
-        res.json({ruta})
+        res.json({ ruta })
     }
 
 }
--- routes/rutas.js
+++ routes/rutas.js
@@ -5,8 +5,10 @@ import { check } from "express-validator"
 const router = Router()
 
 router.get("/", httpRutas.getRutas)
+router.get("/:ruta", httpRutas.getRuta);
 
 router.post("/",[
+    check("nombre", "el nombre es obligatorio").notEmpty().isString().trim(),
     check("origen", "el origen es obligatorio").notEmpty().isString().trim(),
     check("destino", "el destino es obligatorio").notEmpty().trim(),
     check("valor", "el valor es obligatorio").notEmpty().trim(),
